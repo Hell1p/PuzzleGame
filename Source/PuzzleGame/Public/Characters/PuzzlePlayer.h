e42c0b96acc9005205b006d57e92c8f2910c3e83
MelloKell <124379737+MelloKell@users.noreply.github.com>
2023-06-10 16:02:49 +0300

    Movement

--- Source/PuzzleGame/Public/Characters/PuzzlePlayer.h
+++ Source/PuzzleGame/Public/Characters/PuzzlePlayer.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "PuzzlePlayer.generated.h"
@@ -11,19 +10,22 @@ class PUZZLEGAME_API APuzzlePlayer : public ACharacter
 
 public:
 	APuzzlePlayer();
-	
 	virtual void Tick(float DeltaTime) override;
-	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
-	
 	virtual void BeginPlay() override;
 
 private:
+	bool bSprinting;
+	void DirectionalMovement();
 	void MoveForward(float Value);
 	void MoveRight(float Value);
 	void LookUp(float Value);
 	void Turn(float Value);
+	void SprintStart();
+	void SprintEnd();
+	void CrouchStart();
+	void CrouchEnd();
 	
 };
