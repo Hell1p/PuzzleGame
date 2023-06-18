e8077f3af0e8de10dd2c7333755cadadd3c14508
MelloKell <124379737+MelloKell@users.noreply.github.com>
2023-06-18 10:13:40 +0300

    Doors

--- Source/PuzzleGame/Placeables/Door.cpp
+++ Source/PuzzleGame/Placeables/Door.cpp
@@ -8,8 +8,6 @@ ADoor::ADoor()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	DoorMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("DoorMesh"));
-	SetRootComponent(DoorMesh);
 }
 
 void ADoor::BeginPlay()
@@ -20,10 +18,11 @@ void ADoor::BeginPlay()
 void ADoor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+/*
 	if (Button && Button->GetbCubePlaced())
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Emerald, TEXT("DoorOpens"));
 	}
+	*/
 }
 
