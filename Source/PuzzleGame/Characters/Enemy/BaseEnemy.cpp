c4082fff811abb98151a796841e107ba17b07717
Hell1p <106957777+Hell1p@users.noreply.github.com>
2023-06-28 20:41:33 +0300

    Doors fix, enemy

---
+++ Source/PuzzleGame/Characters/Enemy/BaseEnemy.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseEnemy.h"
+#include "AIController.h"
+#include "Perception/PawnSensingComponent.h"
+#include "AITypes.h"
+#include "AI/Navigation/NavigationTypes.h"
+#include "Kismet/GameplayStatics.h"
+#include "PuzzleGame/Characters/Player/PuzzlePlayer.h"
+#include "Navigation/PathFollowingComponent.h"
+
+ABaseEnemy::ABaseEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	PawnSensing = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensing"));
+}
+
+void ABaseEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	EnemyController = Cast<AAIController>(GetController());
+
+	FTimerHandle BeginChaseTimer;
+	GetWorldTimerManager().SetTimer(BeginChaseTimer, this, &ABaseEnemy::OnBeginChaseTimerEnd, 1.f);
+}
+
+void ABaseEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (PuzzlePlayerActor)
+	{
+		if (!InTargetRange(PuzzlePlayerActor, TargetRangeRadius) && !bChasing)
+		{
+			MoveToTarget(PuzzlePlayerActor);
+		}
+		else
+		{
+			bChasing = false;
+		}
+	}
+}
+
+void ABaseEnemy::MoveToTarget(AActor* Target)
+{
+	if (EnemyController == nullptr) return;
+
+	bChasing = true;
+	FAIMoveRequest MoveRequest;
+	MoveRequest.SetGoalActor(Target);
+	MoveRequest.SetAcceptanceRadius(25.f);
+	EnemyController->MoveTo(MoveRequest);
+}
+
+bool ABaseEnemy::InTargetRange(AActor* Target, double Radius)
+{
+	if (Target == nullptr) return false;
+	const double DistanceToTarget = (Target->GetActorLocation() - GetActorLocation()).Size();
+	return DistanceToTarget <= Radius;
+}
+
+void ABaseEnemy::OnBeginChaseTimerEnd()
+{
+	PuzzlePlayerActor = UGameplayStatics::GetActorOfClass(this, APuzzlePlayer::StaticClass());
+	if (PuzzlePlayerActor) MoveToTarget(PuzzlePlayerActor);
+}
